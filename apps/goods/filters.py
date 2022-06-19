51bf5b201951e665497ff49d2ce53f173ffa7a15
你的名字或昵称 <gegyang@gmail.com>
2022-06-19 21:23:49 +0800

    "商品列表页，分页，搜索，过滤，排序"

--- apps/goods/filters.py
+++ apps/goods/filters.py
@@ -5,7 +5,9 @@ from goods.models import Goods
 class GoodsFilter(django_filters.rest_framework.FilterSet):
     price_min = django_filters.NumberFilter(field_name='shop_price',lookup_expr='gte')
     price_max = django_filters.NumberFilter(field_name='shop_price',lookup_expr='lte')
+    shipping_fee = django_filters.BooleanFilter(field_name='ship_free',lookup_expr='isnull')
+    name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
 
     class Meta:
         model = Goods
-        fields = ['price_min','price_max']
+        fields = ['price_min','price_max','shipping_fee','name']
--- apps/goods/views.py
+++ apps/goods/views.py
@@ -1,7 +1,7 @@
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.generics import mixins
 from rest_framework import viewsets
-
+from rest_framework import filters
 from .filters import GoodsFilter
 from .serializers import GoodsSerializer
 from goods.models import Goods
@@ -15,11 +15,15 @@ class GoodsPagination(PageNumberPagination):
 
 class GoodsListViewSet(mixins.ListModelMixin,viewsets.GenericViewSet):
 
-    "list all goods"
+    "商品列表页，分页，搜索，过滤，排序"
 
     queryset = Goods.objects.all()
     serializer_class = GoodsSerializer
     pagination_class = GoodsPagination
-    filter_backends = (DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter,filters.OrderingFilter,)
+    # 排序
+    ordering_fields = {'shop_price'}
+    # 搜索
+    search_fields = {'name','goods_brief'}
     filter_class = GoodsFilter
 
