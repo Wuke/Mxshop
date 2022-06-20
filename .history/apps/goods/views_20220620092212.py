3b9058895ac3949dc84e5ab2eb45faef3e650606
Wuke <gegyang@gmail.com>
2022-06-20 16:55:20 +0800

    simple JWT

--- .history/apps/goods/views_20220620092212.py
+++ .history/apps/goods/views_20220620092212.py
@@ -6,7 +6,6 @@ from .filters import GoodsFilter
 from .serializers import GoodsSerializer, CategorySerializer
 from goods.models import Goods, GoodsCategory
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.authentication import TokenAuthentication
 
 class GoodsPagination(PageNumberPagination):
     page_size = 10
@@ -18,7 +17,6 @@ class GoodsListViewSet(mixins.ListModelMixin,viewsets.GenericViewSet):
     "商品列表页，分页，搜索，过滤，排序"
     queryset = Goods.objects.all()
     serializer_class = GoodsSerializer
-    authentication_classes = (TokenAuthentication,)
     pagination_class = GoodsPagination
     filter_backends = (DjangoFilterBackend, filters.SearchFilter,filters.OrderingFilter,)
     # 排序
